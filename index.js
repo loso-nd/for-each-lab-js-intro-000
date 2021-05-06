eca38170c444c03448986d7be471eb3d38026eb6
Nahum Ducasse <losond8@gmail.com>
2021-05-06 23:14:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+/** Define a function, iterativeLog(), that accepts an array. Call .forEach() on this array, and inside the callback, log each element with the format ${index}: ${element}.
+*/
+
+// write code here //
+function iterativeLog(array){
+  array.forEach((`${element}, ${index}`, arrays) => {
+    
+  }
+}
